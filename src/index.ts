0602929805a41bba65b89512ad8048a9579b6a5a
DMY <147dmy@gmail.com>
2022-11-11 14:30:19 +0800

    feat: add a api to get latest rabby version

--- src/index.ts
+++ src/index.ts
@@ -657,4 +657,9 @@ export class OpenApiService {
     });
     return data;
   };
+
+  getLatestVersion = async (): Promise<{ version: string }> => {
+    const { data } = await this.request.get('/v1/wallet/version');
+    return data;
+  };
 }
