5ae81b2046e19cce7bcbb9aa9fcaa1dc830dda7a
DMY <147dmy@gmail.com>
2024-05-06 18:56:58 +0800

    Feat/gas free (#69)
    * feat: + gasless api
    
    * feat: add 'pre_exec_success' in tx_is_gasless
    
    * feat: add gas_used on tx_is_gasless
    
    * 0.7.14
    
    ---------
    
    Co-authored-by: vvvvvv1vvvvvv <galennnnnnn@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1079,6 +1079,26 @@ export class OpenApiService {
     return data;
   };
 
+  gasLessTxCheck = async ({
+    tx,
+    usdValue,
+    preExecSuccess,
+    gasUsed,
+  }: {
+    gasUsed: number;
+    preExecSuccess: boolean;
+    tx: Tx;
+    usdValue: number;
+  }): Promise<{ is_gasless: boolean }> => {
+    const { data } = await this.request.post('/v1/wallet/tx_is_gasless', {
+      gas_used: gasUsed,
+      pre_exec_success: preExecSuccess,
+      tx,
+      usd_value: usdValue,
+    });
+    return data;
+  };
+
   parseTx = async ({
     chainId,
     tx,
@@ -1444,6 +1464,7 @@ export class OpenApiService {
     req_id?: string;
     tx: Tx;
     push_type: TxPushType;
+    is_gasless: boolean;
     low_gas_deadline?: number;
     origin?: string;
   }): Promise<{ req: TxRequest }> => {
