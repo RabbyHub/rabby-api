32a4b249ecefa2d9fdb235911c2723522f09f014
DMY <147dmy@gmail.com>
2024-08-30 16:35:21 +0800

    feat: gas account

--- src/index.ts
+++ src/index.ts
@@ -59,6 +59,7 @@ import {
   BridgeQuote,
   BridgeHistory,
   ContractInfo,
+  GasAccountCheckResult,
 } from './types';
 
 interface OpenApiStore {
@@ -1654,7 +1655,8 @@ export class OpenApiService {
     req_id?: string;
     tx: Tx;
     push_type: TxPushType;
-    is_gasless: boolean;
+    is_gasless?: boolean;
+    is_gas_account?: boolean;
     log_id: string;
     low_gas_deadline?: number;
     origin?: string;
@@ -2364,6 +2366,144 @@ export class OpenApiService {
       {
         params,
       }
+    );
+    return data;
+  };
+  getGasAccountSignText = async (
+    account_id: string
+  ): Promise<{ text: string }> => {
+    const { data } = await this.request.get('/v1/gas_account/sign_text', {
+      params: {
+        account_id,
+      },
+    });
+    return data;
+  };
+
+  getGasAccountInfo = async (params: {
+    sig: string;
+    id: string;
+  }): Promise<{
+    account: {
+      id: string;
+      balance: number;
+      create_at: number;
+      nonce: number;
+    };
+  }> => {
+    const { data } = await this.request.get('/v1/gas_account', {
+      params: {
+        ...params,
+      },
+    });
+    return data;
+  };
+
+  loginGasAccount = async (params: {
+    sig: string;
+    account_id: string;
+  }): Promise<{
+    success: boolean;
+  }> => {
+    const { data } = await this.request.post('/v1/gas_account/login', {
+      ...params,
+    });
+    return data;
+  };
+
+  logoutGasAccount = async (params: {
+    sig: string;
+    account_id: string;
+  }): Promise<{
+    success: boolean;
+  }> => {
+    const { data } = await this.request.post('/v1/gas_account/logout', {
+      ...params,
+    });
+    return data;
+  };
+
+  getGasAccountTokenList = async (id: string): Promise<TokenItem[]> => {
+    const { data } = await this.request.get('/v1/user/recharge_token_list', {
+      params: {
+        id,
+      },
+    });
+    return data;
+  };
+
+  rechargeGasAccount = async (params: {
+    sig: string;
+    account_id: string;
+    tx_id: string;
+    chain_id: string;
+    amount: number;
+    user_addr: string;
+    nonce: number;
+  }): Promise<{
+    success: boolean;
+  }> => {
+    const { data } = await this.request.post(
+      '/v1/gas_account/recharge',
+      params
+    );
+    return data;
+  };
+
+  withdrawGasAccount = async (params: {
+    sig: string;
+    amount: number;
+    account_id: string;
+  }): Promise<{
+    success: boolean;
+  }> => {
+    const { data } = await this.request.post(
+      '/v1/gas_account/withdraw',
+      params
+    );
+    return data;
+  };
+
+  getGasAccountHistory = async (params: {
+    sig: string;
+    account_id: string;
+    start: number;
+    limit: number;
+  }): Promise<{
+    recharge_list: {
+      amount: number;
+      chain_id: string;
+      create_at: number;
+      gas_account_id: string;
+      tx_id: string;
+      user_addr: string;
+    }[];
+    history_list: {
+      id: string;
+      chain_id: string;
+      amount: number;
+      create_at: number;
+    }[];
+    pagination: {
+      limit: number;
+      start: number;
+      total: number;
+    };
+  }> => {
+    const { data } = await this.request.get('/v1/gas_account/history', {
+      params,
+    });
+    return data;
+  };
+
+  checkGasAccountTxs = async (params: {
+    sig: string;
+    account_id: string;
+    tx_list: Tx[];
+  }): Promise<GasAccountCheckResult> => {
+    const { data } = await this.request.post(
+      '/v1/gas_account/check_txs',
+      params
     );
     return data;
   };
