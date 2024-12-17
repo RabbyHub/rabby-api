b098a84924917edb4f9f5ad170a0b33cf02e4079
Hodor <lost1q84@gmail.com>
2024-12-17 14:37:56 +0800

    feat/token price change (#96)
    * feat: add token price change
    
    * 0.9.4-alpha.0
    
    * fix: ts
    
    * 0.9.4

--- src/index.ts
+++ src/index.ts
@@ -2737,4 +2737,24 @@ export class OpenApiService {
     });
     return data;
   };
+
+  getToken24hPrice = async (params: {
+    chain_id: string;
+    id: string;
+  }): Promise<{ time_at: number; price: number }[]> => {
+    const { data } = await this.request.get('/v1/token/24h_price', {
+      params,
+    });
+    return data;
+  };
+
+  getTokenDatePrice = async (params: {
+    chain_id: string;
+    id: string;
+  }): Promise<{ date_at: string; price: number }[]> => {
+    const { data } = await this.request.get('/v1/token/date_price', {
+      params,
+    });
+    return data;
+  };
 }
