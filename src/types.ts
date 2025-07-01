99d645aaaaa4856cbc326625b85bc695cb7110e1
DMY <147dmy@gmail.com>
2025-07-01 16:19:46 +0800

    feat: default rpcs (#122)
    * feat: add submitTxV2 method and getDefaultRPCs method to OpenApiService; define DefaultRPCRes type
    
    * chore: 0.9.26-beta.0
    
    * fix: stats -> rpcs
    
    * 0.9.26-beta.1

--- src/types.ts
+++ src/types.ts
@@ -1710,3 +1710,15 @@ export interface GasAccountInfo {
   has_iap_order: boolean;
   no_register: boolean;
 }
+
+export type DefaultRPCRes = {
+  message: string;
+  status: string;
+  rpcs: RPCDefaultItem[];
+};
+
+type RPCDefaultItem = {
+  chainId: string;
+  rpcUrl: string[];
+  txPushToRPC: boolean;
+};
