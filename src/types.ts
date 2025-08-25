e262e414dff637817870dd109e56f37a78f4e845
DMY <147dmy@gmail.com>
2025-08-25 16:52:23 +0800

    feat: add buildBridgeTx and extend history support

--- src/types.ts
+++ src/types.ts
@@ -1614,6 +1614,7 @@ export interface BridgeHistory {
   bridge: BridgeItem;
   from_token: TokenItem;
   to_token: TokenItem;
+  to_actual_token: TokenItem;
   quote: {
     pay_token_amount: number;
     receive_token_amount: number;
@@ -1623,7 +1624,7 @@ export interface BridgeHistory {
     receive_token_amount: number;
   };
   detail_url: string;
-  status: 'pending' | 'completed';
+  status: 'pending' | 'completed' | 'failed';
   create_at: number;
   from_tx: {
     tx_id: string;
